164b515b562a3127db6096e30ddc1dda40e7da1d
rubicon44 <rub.b.c44@gmail.com>
2020-09-04 20:28:31 +0900

    delete unnecessary descriptions in App.js

--- src/App.js
+++ src/App.js
@@ -34,20 +34,6 @@ class App extends Component {
               <Route exact path="/top" component={Top} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/registration" component={Registration} />
-            {/* <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header> */}
             </Switch>
           </div>
         </Router>
