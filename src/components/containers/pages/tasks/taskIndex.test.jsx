bd35d9fc35bbb025889edb7b26ef31b81da31ee4
rubicon44 <rub.b.c44@gmail.com>
2022-03-18 11:35:33 +0900

    Create taskIndex.test.jsx

--- src/components/containers/pages/tasks/taskIndex.test.jsx
+++ src/components/containers/pages/tasks/taskIndex.test.jsx
@@ -1,58 +1,36 @@
-import React from 'react';
-import { Router } from 'react-router-dom'
+import React, { useState as useStateMock } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import firebase from 'firebase/app';
-import firebase from "@firebase/rules-unit-testing";
-import initializeTestApp from "@firebase/rules-unit-testing";
 import { TaskIndex } from './taskIndex';
+import { AuthContext } from '../../../../auth/authProvider';
 
-describe("Authentication test in index page", () => {
-  const testProjectId = "test-project-id";
-  const testUid = "testUid";
-  const testEmail = "test@test.com";
-  // const password = "123456";
-  // const apiKey = { apiKey: "test-api-key" };
-  // const authDomain = { authDomain: "test-auth-domain" };
-  // const databaseURL = { databaseURL: "test-database-url" };
-  // const projectId = { projectId: "test-project-id" };
-  // const storageBucket = { storageBucket: "test-storage-bucket" };
-  // const messagingSenderId = { messagingSenderId: "test-messaging-sender-id" };
-
-  // const testAuth = { uid: testUid, email: testEmail }
-
-  // beforeEach(() => {
-    // firebase.initializeTestApp({ projectId: testProjectId, auth: testAuth });
-    // firebase.initializeTestApp({ projectId: testProjectId, auth: testAuth });
-    // firebase.initializeTestApp(testApiKey);
-    // initializeTestApp(testApiKey);
-    firebase.initializeTestApp({
-      projectId: testProjectId,
-      auth: { uid: testUid, email: testEmail }
-    })
-  // });
-});
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}))
 
 describe('TaskIndexコンポーネントテスト', () => {
+  const setState = jest.fn();
+
+  beforeEach(() => {
+    useStateMock.mockImplementation(init => [init, setState])
+  });
+
   it('タイトルが「タスク一覧」になっているか', async () => {
-    render(TaskIndex);
+    const testCurrentUser = { name: 'testUser', email: 'test@test.com' };
+    const [currentUser, setCurrentUser] = useStateMock(testCurrentUser);
+
+    render(
+      <Router>
+        <AuthContext.Provider value={{ currentUser }} >
+          <TaskIndex />
+        </AuthContext.Provider>
+      </Router>
+    );
+
     await waitFor(() => {
-      const title = screen.getByText('タスク一覧');
-      expect(title).toEqual('タスク一覧');
+      const nextTaskCreateLink = screen.getByText('タスク登録');
+      expect(nextTaskCreateLink).toHaveTextContent(/^タスク登録$/);
     });
   });
 });
-
-
-// モックの作成
-//// Firebaseのモックを作成(Firebase Authのユーザーを返すモックを作成する。)
-
-////axiosのモック作成(getTasks()用の、タスクを取得するモックを作成する。)
-
-
-// テスト記述
-//// App.jsにアクセスして、ContextのFirebase認証が行われる(ここでは、認証でtrueが変えるだけで良い。細かい認証テストはContextで行う。)
-
-//// Firebase Authから認証trueが帰ってきた場合、taskIndex.jsxをレンダリングする
-
-//// taskIndex.jsxがレンダリングされたら、useEffectにより、axiosを用いたgetTasks()関数がリクエストされる
