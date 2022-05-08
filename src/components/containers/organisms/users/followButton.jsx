c6018b080aa79a25c88486b6271d49d26834bc1d
rubicon44 <rub.b.c44@gmail.com>
2022-05-08 23:57:03 +0900

    Create follow button change UI

--- src/components/containers/organisms/users/followButton.jsx
+++ src/components/containers/organisms/users/followButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const FollowChange = styled.div`
@@ -11,7 +11,37 @@ const FollowChangeLinkCover = styled.div`
   justify-content: flex-end;
 `;
 
-const FollowChangeLink = styled.a`
+const FollowChangeLinkDone = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  width: 168px;
+  border: 1px solid black;
+  border-color: rgb(207, 217, 222);
+  border-radius: 9999px;
+  font-weight: bold;
+  background-color: rgba(0, 0, 0, 0);
+  cursor: pointer;
+`;
+
+const FollowChangeLinkDoneToUnFollow = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  width: 168px;
+  border: 1px solid black;
+  border-color: rgb(253, 201, 206);
+  border-radius: 9999px;
+  font-weight: bold;
+  background-color: rgba(244, 33, 46, 0.1);
+  cursor: pointer;
+`;
+
+const FollowChangeLinkNone = styled.a`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -23,23 +53,57 @@ const FollowChangeLink = styled.a`
   border-radius: 9999px;
   color: #fff;
   font-weight: bold;
-  // background-color: rgba(0, 0, 0, 0);
   background-color: rgb(15, 20, 25);
   cursor: pointer;
 `;
 
 export function FollowButton() {
-  return (
-    <FollowChange>
-      <FollowChangeLinkCover>
-        <FollowChangeLink
-          onClick={() => {
-            setBioAble(false);
-          }}
-        >
-          <span>フォロー</span>
-        </FollowChangeLink>
-      </FollowChangeLinkCover>
-    </FollowChange>
-  );
+  const [followAble, setFollowAble] = useState(false);
+  const [changeFollowButtonStyle, setChangeFollowButtonStyle] = useState(false);
+
+  if (followAble === true) {
+    return (
+      <FollowChange>
+        <FollowChangeLinkCover>
+          {changeFollowButtonStyle === false ? (
+            <FollowChangeLinkDone
+              onMouseEnter={() => {
+                setChangeFollowButtonStyle(true);
+              }}
+              onClick={() => {
+                setFollowAble(false);
+              }}
+              >
+              <span>フォロー中</span>
+            </FollowChangeLinkDone>
+          ) : (
+            <FollowChangeLinkDoneToUnFollow
+              onMouseLeave={() => {
+                setChangeFollowButtonStyle(false);
+              }}
+              onClick={() => {
+                setFollowAble(false);
+              }}
+              >
+              <span>フォロー解除</span>
+            </FollowChangeLinkDoneToUnFollow>
+          )}
+        </FollowChangeLinkCover>
+      </FollowChange>
+    );
+  } else {
+    return (
+      <FollowChange>
+        <FollowChangeLinkCover>
+          <FollowChangeLinkNone
+            onClick={() => {
+              setFollowAble(true);
+            }}
+          >
+            <span>フォロー</span>
+          </FollowChangeLinkNone>
+        </FollowChangeLinkCover>
+      </FollowChange>
+    );
+  }
 }
