45dd2fec00f40ad7df3acd88bc6896eeefb5b3bd
rubicon44 <rub.b.c44@gmail.com>
2022-10-02 19:12:27 +0900

    Resolved duplicate taskCreatedUser.

--- src/components/containers/organisms/users/userTasksList.jsx
+++ src/components/containers/organisms/users/userTasksList.jsx
@@ -193,6 +193,10 @@ export function UserTasksList() {
     setBioAble(true);
   };
 
+  const uniqueTaskCreatedUsers = Array.from(
+    new Map(taskCreatedUser.map((user) => [user.id, user])).values()
+  );
+
   return (
     <>
       {changeUserNameCheckAble === true && (
@@ -273,9 +277,7 @@ export function UserTasksList() {
             </ListCover>
           ) : (
             userLikedTasks.map((task) => (
-              // todo: この方法はあまり綺麗ではない気がする(特にAPIでのデータの返し方を再考したい)。
-              // 下記方法だと、自分の投稿をいいねした場合、いいねリストに同じタスクが重複してしまう。
-              taskCreatedUser.map((user) => (
+              uniqueTaskCreatedUsers.map((user) => (
                 String(task.user_id) === String(user.id) && (
                   <ListCoverWrapper key={task.id}>
                     <ListCover>
