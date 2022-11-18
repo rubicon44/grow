e9b2bc1e5c190e1ab6d394310d1339a0d1d3e3eb
rubicon44 <rub.b.c44@gmail.com>
2022-11-18 22:00:54 +0900

    Created postTasks mock.

--- src/mocks/db.js
+++ src/mocks/db.js
@@ -1,10 +1,22 @@
 import { factory, primaryKey } from '@mswjs/data';
+import { faker } from '@faker-js/faker';
 
 export const db = factory({
-  // Create a "user" model,
-  user: {
-    id: primaryKey(() => '1'),
-    username: () => '111',
-    nickname: () => 'aaa',
-  },
-})
+  task: {
+    id: primaryKey(faker.datatype.number),
+    title: String,
+    content: Text,
+    status: Number,
+    start_date: String,
+    end_date: String,
+    user: {
+      nickname: faker.name.firstName,
+    }
+  }
+});
+
+// The default tasks created each time you refresh the page.
+db.task.create({ title: "aaa", content: "aaa", status: 0, start_date: "2022-11-15", end_date: "2022-11-16" });
+db.task.create({ title: "bbb", content: "bbb", status: 1, start_date: "2022-11-16", end_date: "2022-11-17" });
+db.task.create({ title: "ccc", content: "ccc", status: 2, start_date: "2022-11-17", end_date: "2022-11-18" });
+db.task.create({ title: "ddd", content: "ddd", status: 2, start_date: "2022-11-18", end_date: "2022-11-19" });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,8 @@
-import { rest } from 'msw'
-import { getTasks } from "./api/tasks";
-import { getTask } from "./api/tasks";
-import { postTasks } from "./api/tasks";
+// import { rest } from 'msw';
+// import { getTasks } from "./api/tasks";
+// import { getTask } from "./api/tasks";
+// import { postTasks } from "./api/tasks";
+import { db } from "./db";
 
 export const handlers = [
   // users
@@ -9,9 +10,13 @@ export const handlers = [
   // auth
 
   // tasks
-  rest.get("http://localhost:3000/tasks", getTasks),
-  rest.get("http://localhost:3000/tasks/:id", getTask),
-  rest.post("http://localhost:3000/tasks", postTasks),
+  // rest.get("http://localhost:3000/tasks", getTasks),
+  // rest.get("http://localhost:3000/tasks/:id", getTask),
+  // rest.post("http://localhost:3000/tasks", postTasks),
+
+  // Create REST API request handlers based on
+  // the "task" database model.
+  ...db.task.toHandlers('rest', 'http://localhost:3000'),
 
   // likes
 
