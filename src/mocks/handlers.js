7c378925a48eeb37580da4f039b54671809c4596
rubicon44 <rub.b.c44@gmail.com>
2022-11-19 15:59:46 +0900

    Created getCurrentUser function mock

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,25 +1,16 @@
 import { rest } from 'msw';
 import { signIn } from "./api/signIn";
+import { getCurrentUser } from "./api/users";
 // import { getTasks } from "./api/tasks";
 // import { getTask } from "./api/tasks";
 // import { postTasks } from "./api/tasks";
 import { db } from "./db";
 
 export const handlers = [
-  // users
   ...db.user.toHandlers('rest', 'http://localhost:3000'),
-
-  // auth
-  rest.post("http://localhost:3000/users/sign_in", signIn),
-
-  // tasks
-  // rest.get("http://localhost:3000/tasks", getTasks),
-  // rest.get("http://localhost:3000/tasks/:id", getTask),
-  // rest.post("http://localhost:3000/tasks", postTasks),
-
-  // Create REST API request handlers based on
-  // the "task" database model.
   ...db.task.toHandlers('rest', 'http://localhost:3000'),
+  rest.post("http://localhost:3000/users/sign_in", signIn),
+  rest.get("http://localhost:3000/:username", getCurrentUser),
 
   // likes
 
