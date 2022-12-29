7d4c414d1b619e80ef65caff4817f732f2c30eae
rubicon44 <rub.b.c44@gmail.com>
2022-12-30 00:17:16 +0900

    Create useSortDOrder.

--- src/components/containers/organisms/tasks/tasksList/TasksListContainer/index.jsx
+++ src/components/containers/organisms/tasks/tasksList/TasksListContainer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useCurrentUserId } from 'hooks/useCurrentUserId';
 import { useCurrentUserName } from 'hooks/useCurrentUserName';
+import { useSortDescendingOrder } from 'hooks/useSortDescendingOrder';
 import { useTasks } from 'hooks/useTasks';
 import { NextButtonLink } from 'components/presentational/atoms/Link/NextButtonLink';
 import { Title } from 'components/presentational/atoms/Title';
@@ -11,11 +12,12 @@ export const TasksListContainer = () => {
   const currentUserId = useCurrentUserId();
   const currentUserName = useCurrentUserName();
   const tasks = useTasks();
+  const sortedDescendingOrderTasks = useSortDescendingOrder(tasks);
   return (
     <>
       <Title>タスク一覧</Title>
       <NextButtonLink text="タスク登録" url="/tasks/create" />
-      <TasksList currentUserId={currentUserId} currentUserName={currentUserName} tasks={tasks}  />
+      <TasksList currentUserId={currentUserId} currentUserName={currentUserName} tasks={sortedDescendingOrderTasks}  />
     </>
   );
 }
--- src/hooks/useTasks.jsx
+++ src/hooks/useTasks.jsx
@@ -2,28 +2,13 @@ import { useEffect, useState } from 'react';
 import { getTasks } from 'infra/api';
 
 export const useTasks = () => {
-  const sortdOrder = (data) => {
-    const list = data;
-    const dOrder = list.sort((a, b) => {
-      if (a.id < b.id) {
-        return 1;
-      }
-      if (a.id > b.id) {
-        return -1;
-      }
-      return 0;
-    });
-    return dOrder;
-  };
-
   const [tasks, setTasks] = useState([]);
   useEffect(async () => {
     let isMounted = true;
     await getTasks()
       .then((response) => {
         if (isMounted) {
-          const dOrderData = sortdOrder(response.data);
-          setTasks(dOrderData);
+          setTasks(response.data);
         }
       })
       .catch();
