c82be4e7a4f38c48bde3db5a2527db0117332c78
rubicon44 <rub.b.c44@gmail.com>
2023-01-02 18:52:28 +0900

    Add comment to useSortDOrder.

--- src/hooks/useSortDescendingOrder.jsx
+++ src/hooks/useSortDescendingOrder.jsx
@@ -1,4 +1,5 @@
 export const useSortDescendingOrder = (data) => {
+  // todo: データ量が多い場合処理が重くなる可能性があるため、後々この機能はAPI側に移行する。
   const list = data;
   if(list) {
     const descendingOrderData = list.sort((a, b) => {
