44d369329802415cb9cc13ceec7d37259ae2876f
rubicon44 <rub.b.c44@gmail.com>
2023-02-27 00:06:34 +0900

    Add error handling to useSignUpForm.

--- src/hooks/useSignUpForm/index.js
+++ src/hooks/useSignUpForm/index.js
@@ -1,24 +1,37 @@
 import { useContext, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from 'auth/AuthProvider';
+import { useGetErrorMessage } from 'hooks/useGetErrorMessage';
 
 export const useSignUpForm = () => {
-  const navigate = useNavigate();
+  const navigateToTasks = useNavigate();
   const location = useLocation();
+  const locationPathName = location.pathname.split('/');
+  const tasksText = locationPathName[locationPathName.length - 1];
   const { signup } = useContext(AuthContext);
+  const { getErrorMessage } = useGetErrorMessage();
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsButtonDisabled(true);
-    const { nickname, username, email, password } = e.target.elements;
-    await signup(nickname.value, username.value, email.value, password.value);
-    setIsButtonDisabled(false);
-    const locationPathName = location.pathname.split('/');
-    const tasksText = locationPathName[locationPathName.length - 1];
-    if(tasksText === "tasks") {
-      window.location.reload();
-    } else {
-      await navigate('/tasks');
+
+    try {
+      const { nickname, username, email, password } = e.target.elements;
+      await signup(nickname.value, username.value, email.value, password.value);
+
+      if(tasksText === "tasks") {
+        window.location.reload();
+      } else {
+        await navigateToTasks('/tasks');
+      };
+    } catch (error) {
+      console.error(`ユーザーのサインアップ中にエラーが発生しました。: `, error);
+      const verbForErrorMessage = `ユーザー`;
+      const objectForErrorMessage = `サインアップ`;
+      getErrorMessage(error, verbForErrorMessage, objectForErrorMessage);
+    } finally {
+      setIsButtonDisabled(false);
     };
   };
   return { handleSubmit, isButtonDisabled };
