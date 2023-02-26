8e74dd3509f31824a148c813cf9be7905e3c9030
rubicon44 <rub.b.c44@gmail.com>
2023-02-26 21:55:55 +0900

    Add error handling to useSignOut & Change Func name -Func to handle-.

--- src/components/containers/organisms/common/LogOutButton/index.jsx
+++ src/components/containers/organisms/common/LogOutButton/index.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Button } from 'components/presentational/atoms/Button';
 
-export const LogOutButton = ({ signoutFunc, text }) => {
-  return <DangerButton onClick={signoutFunc}>{text}</DangerButton>;
+export const LogOutButton = ({ handleSignout, text }) => {
+  return <DangerButton onClick={handleSignout}>{text}</DangerButton>;
 };
 
 // const DangerButton = styled(props => <Button {...props} />)`
--- src/hooks/useSignOut/index.js
+++ src/hooks/useSignOut/index.js
@@ -1,14 +1,23 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from 'auth/AuthProvider';
+import { useGetErrorMessage } from 'hooks/useGetErrorMessage';
 
 export const useSignOut = () => {
   const navigate = useNavigate();
   const { signout } = useContext(AuthContext);
+  const { getErrorMessage } = useGetErrorMessage();
 
-  const signoutFunc = async () => {
-    await navigate('/');
-    await signout();
+  const handleSignout = async () => {
+    try {
+      await signout();
+      await navigate('/');
+    } catch (error) {
+      console.error(`ユーザーのログアウト中にエラーが発生しました。: `, error);
+      const verbForErrorMessage = `ユーザー`;
+      const objectForErrorMessage = `ログアウト`;
+      getErrorMessage(error, verbForErrorMessage, objectForErrorMessage);
+    };
   };
-  return { signoutFunc };
+  return { handleSignout };
 };
