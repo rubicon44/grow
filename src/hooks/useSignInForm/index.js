6043d5ca6476ce06038bfc6fa4d1dcd2fed82e6b
rubicon44 <rub.b.c44@gmail.com>
2023-02-26 22:59:02 +0900

    Add error handling to useSignInForm.

--- src/hooks/useSignInForm/index.js
+++ src/hooks/useSignInForm/index.js
@@ -1,24 +1,37 @@
 import { useContext, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from 'auth/AuthProvider';
+import { useGetErrorMessage } from 'hooks/useGetErrorMessage';
 
 export const useSignInForm = () => {
-  const navigate = useNavigate();
+  const navigateToTasks = useNavigate();
   const location = useLocation();
+  const locationPathName = location.pathname.split('/');
+  const tasksText = locationPathName[locationPathName.length - 1];
   const { signin } = useContext(AuthContext);
+  const { getErrorMessage } = useGetErrorMessage();
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsButtonDisabled(true);
-    const { email, password } = e.target.elements;
-    await signin(email.value, password.value);
-    setIsButtonDisabled(false);
-    const locationPathName = location.pathname.split('/');
-    const tasksText = locationPathName[locationPathName.length - 1];
-    if(tasksText === "tasks") {
-      window.location.reload();
-    } else {
-      await navigate('/tasks');
+
+    try {
+      const { email, password } = e.target.elements;
+      await signin(email.value, password.value);
+
+      if(tasksText === "tasks") {
+        window.location.reload();
+      } else {
+        await navigateToTasks('/tasks');
+      };
+    } catch (error) {
+      console.error(`ユーザーのログイン中にエラーが発生しました。: `, error);
+      const verbForErrorMessage = `ユーザー`;
+      const objectForErrorMessage = `ログイン`;
+      getErrorMessage(error, verbForErrorMessage, objectForErrorMessage);
+    } finally {
+      setIsButtonDisabled(false);
     };
   };
   return { handleSubmit, isButtonDisabled };
--- src/hooks/useSignOut/index.js
+++ src/hooks/useSignOut/index.js
@@ -4,14 +4,14 @@ import { AuthContext } from 'auth/AuthProvider';
 import { useGetErrorMessage } from 'hooks/useGetErrorMessage';
 
 export const useSignOut = () => {
-  const navigate = useNavigate();
+  const navigateToTop = useNavigate();
   const { signout } = useContext(AuthContext);
   const { getErrorMessage } = useGetErrorMessage();
 
   const handleSignout = async () => {
     try {
       await signout();
-      await navigate('/');
+      await navigateToTop('/');
     } catch (error) {
       console.error(`ユーザーのログアウト中にエラーが発生しました。: `, error);
       const verbForErrorMessage = `ユーザー`;
