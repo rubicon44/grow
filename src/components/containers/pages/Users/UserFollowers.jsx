55a1cc281d8a67d2e9f7c54f4cfe25956944ac03
rubicon44 <rub.b.c44@gmail.com>
2023-03-05 15:15:57 +0900

    test

--- src/components/containers/pages/Users/UserFollowers.jsx
+++ src/components/containers/pages/Users/UserFollowers.jsx
@@ -1,6 +1,5 @@
 import { UserFollowersTemplate } from '../../templates/users/userFollowersTemplate';
 
-// Change file name.
 export const UserFollowers = () => {
   return <UserFollowersTemplate />;
 };
