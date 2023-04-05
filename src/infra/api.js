f894630353288b99c11689ff7f9fb8abf046978b
rubicon44 <rub.b.c44@gmail.com>
2023-04-05 20:57:16 +0900

    fix cross-origin error.

--- src/infra/api.js
+++ src/infra/api.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.withCredentials = true;
 const tokenAuth = localStorage.getItem('token');
 axios.defaults.headers.common.Authorization = tokenAuth;
 
