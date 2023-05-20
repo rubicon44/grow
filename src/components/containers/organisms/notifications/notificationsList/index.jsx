8f793ecf159f97318678cdb7df77a8b20c7c1745
rubicon44 <rub.b.c44@gmail.com>
2023-05-20 22:25:42 +0900

    Move titleWithBackArrow into templates in notifications.

--- src/components/containers/organisms/notifications/notificationsList/index.jsx
+++ src/components/containers/organisms/notifications/notificationsList/index.jsx
@@ -1,15 +1,11 @@
 import PropTypes from "prop-types";
 import { NotificationListWrapperContainer } from "./NotificationListWrapperContainer";
-import { TitleWithBackArrowHeader } from "../../../../presentational/molecules/Header/TitleWithBackArrowHeader";
 
 export const NotificationsList = ({ currentUserName, notificationsData }) => (
-  <>
-    <TitleWithBackArrowHeader>通知一覧</TitleWithBackArrowHeader>
-    <NotificationListWrapperContainer
-      currentUserName={currentUserName}
-      notificationsData={notificationsData}
-    />
-  </>
+  <NotificationListWrapperContainer
+    currentUserName={currentUserName}
+    notificationsData={notificationsData}
+  />
 );
 
 NotificationsList.propTypes = {
--- src/components/containers/templates/Notifications/NotificationsTemplate.jsx
+++ src/components/containers/templates/Notifications/NotificationsTemplate.jsx
@@ -1,8 +1,10 @@
 import { MainWithHeader } from "../MainWithHeader";
 import { NotificationsListContainer } from "../../organisms/notifications/NotificationsListContainer";
+import { TitleWithBackArrowHeader } from "../../../presentational/molecules/Header/TitleWithBackArrowHeader";
 
 export const NotificationsTemplate = () => (
   <MainWithHeader>
+    <TitleWithBackArrowHeader>通知一覧</TitleWithBackArrowHeader>
     <NotificationsListContainer />
   </MainWithHeader>
 );
