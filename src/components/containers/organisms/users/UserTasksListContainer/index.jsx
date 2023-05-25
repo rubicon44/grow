9b74f6dab2bd1e724ea39ca48913f05e1a39a97d
rubicon44 <rub.b.c44@gmail.com>
2023-05-25 18:36:01 +0900

    Move declaration of useUserData to organisms in users.

--- src/components/containers/organisms/users/UserTasksListContainer/index.jsx
+++ src/components/containers/organisms/users/UserTasksListContainer/index.jsx
@@ -1,22 +1,29 @@
-import PropTypes from "prop-types";
 import { useCurrentUserId } from "../../../../../hooks/useCurrentUserId";
 import { useMoveToFollowers } from "../../../../../hooks/useMoveToFollowers";
 import { useMoveToFollowings } from "../../../../../hooks/useMoveToFollowings";
 import { useShowPopup } from "../../../../../hooks/useShowPopup";
+import { useUserData } from "../../../../../hooks/useUserData";
 import { UserTasksList } from "../userTasksList";
 
-export const UserTasksListContainer = ({
-  setCheckUserNameChange,
-  setUserData,
-  userData,
-  currentPath,
-}) => {
+export const UserTasksListContainer = () => {
   const { showPopup } = useShowPopup();
   const currentUserId = useCurrentUserId();
+
+  const {
+    error,
+    loading,
+    setCheckUserNameChange,
+    setUserData,
+    userData,
+    currentPath,
+  } = useUserData();
 
   // todo: useMoveTo~は、API通信用のHooksと見間違える可能性があるため、名称変更した方が良い？
   const { moveToFollowers } = useMoveToFollowers(userData);
   const { moveToFollowings } = useMoveToFollowings(userData);
+
+  if (error) return <>Error...</>;
+  if (loading) return <>Loading...</>;
 
   return (
     <UserTasksList
@@ -30,39 +37,4 @@ export const UserTasksListContainer = ({
       currentPath={currentPath}
     />
   );
-};
-
-UserTasksListContainer.propTypes = {
-  setCheckUserNameChange: PropTypes.func.isRequired,
-  setUserData: PropTypes.func.isRequired,
-  userData: PropTypes.shape({
-    id: PropTypes.number,
-    bio: PropTypes.string,
-    email: PropTypes.string,
-    likedTasks: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        userId: PropTypes.number,
-        content: PropTypes.string,
-        endDate: PropTypes.string,
-        startDate: PropTypes.string,
-        status: PropTypes.number,
-        title: PropTypes.string,
-      })
-    ),
-    nickname: PropTypes.string,
-    tasks: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        userId: PropTypes.number,
-        content: PropTypes.string,
-        endDate: PropTypes.string,
-        startDate: PropTypes.string,
-        status: PropTypes.number,
-        title: PropTypes.string,
-      })
-    ),
-    username: PropTypes.string,
-  }).isRequired,
-  currentPath: PropTypes.string.isRequired,
 };
--- src/components/containers/templates/Users/UserShowTemplate.jsx
+++ src/components/containers/templates/Users/UserShowTemplate.jsx
@@ -1,28 +1,8 @@
 import { MainWithHeader } from "../MainWithHeader";
 import { UserTasksListContainer } from "../../organisms/users/UserTasksListContainer";
-import { useUserData } from "../../../../hooks/useUserData";
 
-export const UserShowTemplate = () => {
-  // todo: カスタムHooksの呼び出し位置を検討(headerのh1のため)。
-  const {
-    error,
-    loading,
-    setCheckUserNameChange,
-    setUserData,
-    userData,
-    currentPath,
-  } = useUserData();
-
-  if (error) return <>Error...</>;
-  if (loading) return <>Loading...</>;
-  return (
-    <MainWithHeader title={userData.nickname}>
-      <UserTasksListContainer
-        setCheckUserNameChange={setCheckUserNameChange}
-        setUserData={setUserData}
-        userData={userData}
-        currentPath={currentPath}
-      />
-    </MainWithHeader>
-  );
-};
+export const UserShowTemplate = () => (
+  <MainWithHeader>
+    <UserTasksListContainer />
+  </MainWithHeader>
+);
