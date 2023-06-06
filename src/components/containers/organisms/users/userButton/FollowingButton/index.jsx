94100863825ef86d3a241ff326042b6291690943
rubicon44 <rub.b.c44@gmail.com>
2023-06-06 17:36:03 +0900

    Create common button component in UserTasks.

--- src/components/containers/organisms/users/userButton/FollowingButton/index.jsx
+++ src/components/containers/organisms/users/userButton/FollowingButton/index.jsx
@@ -1,29 +1,17 @@
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import { UserTasksButton } from "../../ui/UserTasksButton";
 
 export const FollowingButton = ({ setChangeFollowButtonStyleToTrueFunc }) => (
-  // todo: buttonコンポーネントの使用を検討。
-  <FollowChangeLinkDone onMouseEnter={setChangeFollowButtonStyleToTrueFunc}>
+  <UserTasksButton
+    onMouseEnter={setChangeFollowButtonStyleToTrueFunc}
+    color="black"
+    isBold="true"
+    size="small"
+  >
     <span>フォロー中</span>
-  </FollowChangeLinkDone>
+  </UserTasksButton>
 );
 
 FollowingButton.propTypes = {
   setChangeFollowButtonStyleToTrueFunc: PropTypes.func.isRequired,
 };
-
-// todo: Buttonをコンポーネント化
-const FollowChangeLinkDone = styled.a`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 32px;
-  width: 118px;
-  border: 1px solid black;
-  border-color: rgb(207, 217, 222);
-  border-radius: 9999px;
-  font-weight: bold;
-  background-color: rgba(0, 0, 0, 0);
-  cursor: pointer;
-`;
