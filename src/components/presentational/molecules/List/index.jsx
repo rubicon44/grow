968ad4c23d46fe35c5d6b64da6d4192a27ff5155
rubicon44 <rub.b.c44@gmail.com>
2023-06-28 07:32:41 +0900

    Add markdown to task.content in front-end.

--- src/components/presentational/molecules/List/index.jsx
+++ src/components/presentational/molecules/List/index.jsx
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
+// Hooks呼び出しを許容
+import { useListConverter } from "../../../../hooks/useListConverter";
 import { mediaquery } from "../../../../assets/styles/variable";
 
-export const List = ({ title, content }) => (
-  // TODO: httpsにおいてXSSできないことを確認する
-  // const userInputText1 = `javascript: console.log(document.cookie)`;
-  <BaseList>
-    <h3>{title}</h3>
-    {/* <Link to={userInputText1}>{title}</Link> */}
-    <p>{content}</p>
-  </BaseList>
-);
+export const List = ({ title, content }) => {
+  const htmlContent = useListConverter(content);
+  return (
+    // TODO: httpsにおいてXSSできないことを確認する
+    // const userInputText1 = `javascript: console.log(document.cookie)`;
+    <BaseList>
+      <h3>{title}</h3>
+      {/* <Link to={userInputText1}>{title}</Link> */}
+      <BaseListContent>
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </BaseListContent>
+    </BaseList>
+  );
+};
 
 List.propTypes = {
   content: PropTypes.string.isRequired,
@@ -36,4 +43,18 @@ const BaseList = styled.div`
       min-height: 250px;
     `}
   }
+`;
+
+const BaseListContent = styled.div`
+  max-width: 510px;
+  min-height: 200px;
+  margin: 10px 0 5px;
+  padding: 10px;
+  border: 1px solid #bbb;
+  white-space: pre-wrap;
+  border-radius: 5px;
+  box-sizing: border-box;
+  ${mediaquery.desk`
+    min-height: 250px;
+  `}
 `;
---
+++ src/hooks/useListConverter/index.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { html } from "remark-html";
+import { remark } from "remark";
+
+export const useListConverter = (content) => {
+  const [htmlContent, setHtmlContent] = useState("");
+
+  useEffect(() => {
+    const convertList = (content) => {
+      const lines = content.split("\n");
+      const convertedLines = lines.map((line) => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith("#")) {
+          const headingLevel = Math.min(trimmedLine.split("#").length - 1, 6);
+          const text = trimmedLine.replace(/#/g, "").trim();
+          const headingStyle = `
+            font-size: ${24 - (headingLevel - 1) * 2}px;
+            font-weight: bold;
+          `;
+          return `<h${headingLevel} style="${headingStyle}">${text}</h${headingLevel}>`;
+        }
+        if (trimmedLine.startsWith("- [ ]")) {
+          const text = trimmedLine
+            .replace("- [ ]", "")
+            .trim()
+            .replace(/\\/g, "&#92;");
+          return `<label style="display: flex;"><input type="checkbox" style="width: 13px; margin-right: 5px;" />${text}</label>`;
+        }
+        return line.replace(/^- /, "・");
+      });
+      const filteredLines = convertedLines.filter((line) => line.trim() !== ""); // 空行をフィルタリング
+      return filteredLines.join(""); // 改行コードを除去して連結
+    };
+
+    const convertedContent = convertList(content);
+    const processedContent = remark().use(html).processSync(convertedContent);
+    const newHtmlContent = processedContent.toString();
+
+    setHtmlContent(newHtmlContent);
+  }, [content]);
+
+  return htmlContent;
+};
